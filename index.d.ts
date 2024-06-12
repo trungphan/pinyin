bd21f935ca05138951662df8d5ee74d2239907bd
Trung Phan <trung.phanchi@gmail.com>
2024-06-11 18:33:11 -0700

    Add CommonJs and Typescript

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export function compactPinyin(pinyin: string): string;
+export function compactPinyin1(pinyin: string): string;
+export function convertToPinyin(pinyin: string): string;
+export function convertToPinyin1(pinyin: string): string;
+export function normalizePinyin(pinyin: string): string;
+export function normalizePinyin1(pinyin: string): string;
+export function pinyinForPolly(pinyin: string): string;
